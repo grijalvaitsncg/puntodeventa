5ba3b2a2c344b4f7f5fe90c33e92e6b35bf87fae
grijalvaitsncg <89266451+grijalvaitsncg@users.noreply.github.com>
2026-02-26 14:53:18 -0600

    Cambio de pantalla

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -7,4 +7,15 @@ class LoginController:
         self.window.btn_login.clicked.connect(self.handle_login)
     
     def handle_login(self):
-        print("Login button clicked 👌")
+        username = self.window.txt_username.text()
+        password = self.window.txt_password.text()
+        if username == "admin" and password == "123":
+            self.window.login_successfull.emit()
+            print("Login correcto 👌")
+        else:
+            #print("Login incorrecto ❌")
+            QtWidgets.QMessageBox.warning(
+                self.window,
+                "Abarrotes TEC - ERROR",
+                "Login incorrecto ❌"
+            )
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from PyQt6 import QtWidgets, uic
 import sys
 from controllers.login_controller import LoginController
+from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtGui import QPalette
 
 #pip install pyqt6 pyqt6-tools
 
 class Login(QtWidgets.QMainWindow):
+    login_successfull = pyqtSignal()
+
     def __init__(self):
         super().__init__()
         uic.loadUi("./views/login.ui", self)
         self.controller = LoginController(self, self)
+        self.apply_theme()
+    
+    def apply_theme(self):
+        is_dark = self.palette().color(QPalette.ColorRole.Window).lightness() <128
+        print(f"dark: {is_dark} { self.palette().color(QPalette.ColorRole.Window).lightness()}")
         
 class Sell(QtWidgets.QMainWindow):
     def __init__(self):
@@ -19,8 +28,14 @@ class AppManager:
     def __init__(self):
         self.login_window=Login()
         self.sell_window = Sell()
+        self.login_window.login_successfull.connect(self.show_main_window)
         #muestra la pantalla
         self.login_window.show()
+
+
+    def show_main_window(self):
+        self.sell_window.show()#mostrar ventana de venta
+        self.login_window.close()#cerramos la ventana
 
 app = QtWidgets.QApplication(sys.argv)
 manager = AppManager()
